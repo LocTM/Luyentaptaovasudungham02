624553d134bc1167bb740b2048fb84e908873858
LocTM <tranminhloc.anhk@gmail.com>
2023-11-26 20:04:35 +0700

    main

---
+++ index.js
@@ -0,0 +1,66 @@
+// BT1
+// function square(a){
+//     return a*a;
+// }
+// let result = square(2);
+// console.log(result)
+
+// BT2
+// let Pi = 3.14;
+// let a = + prompt("Nhập vào đây bán kính của hình tròn: ")
+// function calC(a){
+//     return a * 2 * Pi;
+// }
+// function calS(a){
+//     return a * a * Pi;
+// }
+// let result = calC(a);
+// let result1 = calS(a);
+// console.log("Chu vi hình tròn là: " + result)
+// console.log("Diện tích hình tròn là: " + result1)
+
+// // BT3
+// let a = 10
+// function calF(){
+//     let factorial = 1;
+//     for (let i = 2; i <= a; i++) {
+//         factorial *= i;
+//     }
+//     return factorial;
+// }
+// let result = calF(a);
+// console.log(result);
+
+// BT4
+// let a = prompt("Nhập vào đây ký tự cần kiểm tra")
+// function isNumber(){
+//     if (!isNaN(a)) {
+//     console.log("True");
+//     } else {
+//     console.log("False");
+//     }
+// }
+// isNumber();
+
+// //BT5
+// let a = prompt("Nhập vào số a");
+// let b = prompt("Nhập vào số b");
+// let c = prompt("Nhập vào số c");
+//
+// function findMin(a,b,c){
+//     return Math.min(a,b,c);
+// }
+// let min = findMin(a,b,c)
+// alert("Số nhỏ nhất trong 3 số đó là: "+min)
+
+// BT6
+// let a = +prompt("Nhập vào đây số cần kiểm tra:")
+// function isInteger(){
+//     if(a > 0 && a % 1 === 0 ){
+//         alert("True")
+//     }
+//     else{
+//         alert("False")
+//     }
+// }
+// isInteger();
